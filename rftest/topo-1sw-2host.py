9f9ef5a1a6a6d0100982a3c0f8cae6a633f74411
routeflow <routeflow@routeflow.(none)>
2013-11-16 19:03:44 -0200

    Instâncias do mongo estão funcionando

---
+++ rftest/topo-1sw-2host.py
@@ -0,0 +1,30 @@
+""" rftest3 topology
+
+authors: Davi G. Peterlini (davip@cpqd.com.br)
+
+One switches connected in mesh topology plus a host for each switch:
+
+       h1 --- sT ---- h2
+
+"""
+
+from mininet.topo import Topo
+
+class rftest3(Topo):
+    "RouteFlow Demo Setup"
+
+    def __init__( self, enable_all = True ):
+        "Create custom topo."
+
+        Topo.__init__( self )
+
+        switch = self.addSwitch("s1")
+
+        for h in range(2):
+            host = self.addHost('h%s' % (h + 1), 
+                                ip="172.31.%s.100/24" % (h + 1),
+                                defaultRoute="gw 172.31.%s.1" % (h + 1))
+            
+            self.addLink(host, switch)
+
+topos = { 'rftest3': ( lambda: rftest3() ) }
